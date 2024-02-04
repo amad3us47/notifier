b169df2c666d6bc16d0e0614761cdfb0b78593ce
Aman <131179388+amad3us47@users.noreply.github.com>
2024-02-04 09:31:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,8 @@
 import os
 import time
-import requests
-import filecmp
-import shutil
-import difflib
-from datetime import datetime
-file1="/home/runner/work/automation/automation/o.txt"
-file2="/home/runner/work/automation/automation/new.txt"
-header = {
-    'authorization': 'MTE3NzI2NjM3MzYxODA2MTQ3Nw.Gfs-tu.QP_yvc0aqe9vZPWhV6AjDZHWmK6jFR9naBD3L0'
-}
-#current=os.path.getsize('C:\\Users\\chotu\\Desktop\\stocks\\o.txt')
-for i in range(10000000):
-    os.system('curl https://raw.githubusercontent.com/arkadiyt/bounty-targets-data/main/data/domains.txt -o o.txt')
-    with open("/home/runner/work/automation/automation/o.txt",'r') as file1,open("/home/runner/work/automation/automation/new.txt",'r') as file2:
-        diff=difflib.unified_diff(file1.readlines(),file2.readlines(),lineterm='')
-        for line in diff:
-            print(line,datetime.now())
-            time=datetime.now()
-            line=line+""+str(time)
-            payload = {'content':line}
-            r = requests.post("https://discord.com/api/v9/channels/1203177915752251435/messages", 
-                    data=payload, headers=header)
-
-    time.sleep(300)
-    file1=file2
-    #new=os.path.getsize('C:\\Users\\chotu\\Desktop\\stocks\\o.txt')
-    #if new > current:
-        #current=new
-        #os.system('curl -d "new added check now" ntfy.sh/bbrsmend')
+current=os.path.size('/home/runner/work/automation/automation/new.txt')
+os.system('curl https://raw.githubusercontent.com/arkadiyt/bounty-targets-data/main/data/domains.txt -o o.txt')
+new=os.path.getsize('/home/runner/work/automation/automation/o.txt')
+if new > current:
+        current=new
+        os.system('curl -d "new added check now" ntfy.sh/bbrsmend')
