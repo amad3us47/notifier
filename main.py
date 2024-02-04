23a243298a32be0ae710b16a7b41ef861db7ec65
Aman <131179388+amad3us47@users.noreply.github.com>
2024-02-04 10:32:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import os
 import time
-current=os.path.size('/home/runner/work/automation/automation/new.txt')
+from pathlib import Path
+path_current=Path('/home/runner/work/automation/automation/new.txt')
+path_new=Path('/home/runner/work/automation/automation/o.txt')
+current=os.path.getsize(path_current)
 os.system('curl https://raw.githubusercontent.com/arkadiyt/bounty-targets-data/main/data/domains.txt -o o.txt')
-new=os.path.getsize('/home/runner/work/automation/automation/o.txt')
+new=os.path.getsize(path_new)
 if new > current:
         current=new
         os.system('curl -d "new added check now" ntfy.sh/bbrsmend')
